1974d804fc9c6c1eaf07b2546b526e87de797130
Prathamesh <prathameshaj2004@gmail.com>
2025-04-07 10:53:42 +0530

    DL

--- Experiment-2-Implement character and digit recognition using ANN/RandInitialise.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def initialise(a, b):
-    epsilon = 0.15
-    c = np.random.rand(a, b + 1) * (2 * epsilon) - epsilon  # Randomly initialises values of thetas between [-epsilon, +epsilon]
-    return c
